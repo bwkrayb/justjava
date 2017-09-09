b3995b2114ce11c1c85fb30c916463faf6349ad9
bwkrayb <Bk_5229>
2017-09-09 08:28:44 -0500

    Added nested horizontal layout

--- app/src/main/java/com/example/bwkrayb/justjava/MainActivity.java
+++ app/src/main/java/com/example/bwkrayb/justjava/MainActivity.java
@@ -24,11 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
 
-    int total = 0;
-
-    double taxcost = 0;
-
-    double tax = 0.076;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,11 +54,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        total = quantity * 5;
-        displayPrice(total);
-        taxcost = total * tax;
-        displayTax(taxcost);
-        displayTotal(total + taxcost);
+        displayPrice(quantity * 5);
+        String priceMessage="Free";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -83,19 +76,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method displays the given tax on the screen.
+     * This method displays the given text on the screen.
      */
-    private void displayTax(double number) {
-        TextView priceTextView = (TextView) findViewById(R.id.tax_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance(Locale.US).format(number));
-    }
-
-    /**
-     * This method displays the given total on the screen.
-     */
-    private void displayTotal(double number) {
-        TextView priceTextView = (TextView) findViewById(R.id.total_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance(Locale.US).format(number));
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
 }

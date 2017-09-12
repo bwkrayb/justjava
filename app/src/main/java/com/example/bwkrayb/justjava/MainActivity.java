7149a758a07ba9137121c418bf0eb03d6578ec59
bwkrayb <bwkrayb@gmail.com>
2017-09-11 19:16:10 -0500

    Didn't fix bug for Lollipop... but oh well.

--- app/src/main/java/com/example/bwkrayb/justjava/MainActivity.java
+++ app/src/main/java/com/example/bwkrayb/justjava/MainActivity.java
@@ -13,8 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.bwkrayb.justjava.R;
-
 import java.util.Locale;
 
 /**
